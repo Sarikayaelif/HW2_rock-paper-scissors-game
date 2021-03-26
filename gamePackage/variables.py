5774d546fb569314b3ef80b75c138018c06192b0
Sarikayaelif <77562114+Sarikayaelif@users.noreply.github.com>
2021-03-26 19:12:53 -0400

    Update variables.py

--- gamePackage/variables.py
+++ gamePackage/variables.py
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-=======
 
->>>>>>> main
+
+
 choices = ["rock", "paper", "scissors"]
 total_lives = 3
 your_lives = total_lives
 my_lives = total_lives
-<<<<<<< HEAD
-your_choice = False
-=======
+
 your_choice = False
 first_entry = True
->>>>>>> main
+
